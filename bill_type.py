f56b2057a516f078f751969fefed7b60c3cc2da3
AndreaOrlando23 <64850221+AndreaOrlando23@users.noreply.github.com>
2021-03-20 19:55:51 +0100

    Created BillType class

---
+++ bill_type.py
@@ -0,0 +1,21 @@
+class BillType:
+    bill_types = {
+        1: "Ambata",
+        2: "Ambo",
+        3: "Terno",
+        4: "Quaterna",
+        5: "Cinquina",
+    }
+
+    def __init__(self, bill=0):
+        self.bill = bill
+
+    def city_check(self):
+        if self.bill in BillType.bill_types.keys():
+            return True
+        return False
+
+    @staticmethod
+    def print_cities():
+        for key, value in BillType.bill_types.items():
+            print(key, value)
