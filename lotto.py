fd526156edfa41b736f2a1986070a5329e8d2617
AndreaOrlando23 <64850221+AndreaOrlando23@users.noreply.github.com>
2021-03-27 18:11:02 +0100

    Add pick_bet method to Lotto class

--- lotto.py
+++ lotto.py
@@ -12,15 +12,36 @@ class Lotto:
         print("> Pick the 'ruota' (aka city) by the corresponding number:")
         City.print_cities()
 
-        city = int(input("> Enter the corresponding number to the city: "))
+        city = int(input("> Enter the corresponding number of the city: "))
+        c = City(city)
+        if c.get_city(city):
+            self.city = c.get_city(city)
+            return self.city
+        else:
+            return "Enter a number between 1 to 12"
 
+    def pick_bet(self):
+        print("> Pick the bet type by the corresponding number:")
+        BetType.print_bet()
+
+        bet = int(input("> Enter the corresponding number of the bet type: "))
+        b = BetType(bet)
+        if b.get_bet(bet):
+            self.bet = b.get_bet(bet)
+            return self.bet
+        else:
+            return "Enter a number between 1 to 5"
+
+        '''
         if City.check_city(city):
             c = City(city)
             self.city = c.get_city(city)
-
+        
         else:
             print("Enter a number between 1 to 12")
+        '''
 
 
 lotto = Lotto()
 print(lotto.pick_city())
+print(lotto.pick_bet())
