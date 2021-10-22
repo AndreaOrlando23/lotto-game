a73121577bada02f7d548d08e9aed59ef7aef782
Andrea Orlando <andrea.orlando87@gmail.com>
2021-10-22 12:21:12 +0200

    feat: implement class Ticket

--- ticket.py
+++ ticket.py
@@ -1,1 +1,51 @@
-# TODO
+from city import City
+from bet_type import BetType
+
+
+class Ticket:
+
+    def __init__(self, num_tickets, city, bet, num_extracted=[]):
+        self.num_tickets = num_tickets
+        self.city = city
+        self.bet = bet
+        self.num_extracted = num_extracted
+
+    def print_ticket(self):
+        table_line = '+'+'-'*48+'+'
+        header = '|{:^48}|'.format('TICKET ' + str(self.num_tickets))
+        city = '|{:^48}|'.format('City: ' + self.city)
+        bet = '|{:^48}|'.format('Bet: ' + self.bet)
+        numbers = '|{:^48}|'.format(' '.join([str(elem) for elem in self.num_extracted]))
+
+        return f"""
+        {table_line}
+        {header}
+        {table_line}
+        {city}
+        {bet}
+        {numbers}
+        {table_line}
+        """
+
+
+# Test
+if __name__ == '__main__':
+    city = City()
+    print("Select the number of the city you want to game with:")
+    city.print_cities()
+        
+    test_city = int(input("Number: "))
+    get_city = city.get_city(test_city)
+    
+
+    bet = BetType()
+    print("Select the number of the bet you want to game with:")
+    bet.print_bet()
+
+    test_bet = int(input("Number: "))
+    get_bet = bet.get_bet(test_bet)
+
+    ticket = Ticket(1, get_city, get_bet, [1, 2, 3, 4, 5])
+    print(ticket.print_ticket())
+
+
