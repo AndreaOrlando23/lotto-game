e6a110aa5863d63d4d9582b5182a4f7e863246c1
Andrea Orlando <andrea.orlando87@gmail.com>
2023-04-21 12:51:56 +0200

    [FEAT] implement get_extractions() in Extraction class

--- lotto/extraction.py
+++ lotto/extraction.py
@@ -1,11 +1,34 @@
-from lotto.city import City
+from city import City
 import random
 
 
 class Extraction:
+    extractions = {}
 
-    def __init__(self, num_extracted):
+    def __init__(self, num_extracted=5):
         self.num_extracted = num_extracted
     
 
-    # TODO
+    def get_extractions(self, cities):
+        for city in cities.values():
+            # self.extractions[city] = random.sample(range(1, 91), self.num_extracted)
+            self.extractions.update({city: random.sample(range(1, 91), self.num_extracted)})
+        return self.extractions
+    
+
+    def output(self):
+        # TODO
+        for city, num in self.extractions.items():
+            print(city, num)
+    
+
+
+
+
+# Test
+if __name__ == '__main__':
+    cities = City().cities
+    extraction = Extraction(5)
+
+    print(extraction.get_extractions(cities))
+    print(extraction.output())
